456440840436712cdbc9ade8bad3cbd7fac26f87
Zhou <2675416482@qq.com>
2015-04-23 19:38:04 +0800

    hello WOrld

---
+++ hello_git/hello_git/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello_git
+//
+//  Created by ZhouJiaming on 15/4/23.
+//  Copyright (c) 2015年 ZhouJiaming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
